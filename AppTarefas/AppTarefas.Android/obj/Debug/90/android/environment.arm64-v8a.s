95dcfd20f4dd00102d79663b266758b900e08b55
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-20 16:08:53 -0300

    StatusBar

--- AppTarefas/AppTarefas.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ AppTarefas/AppTarefas.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"61858ca1-8dac-4de5-afd6-28ffb61dfcfe"
+	.asciz	"8eaee6c1-41d5-4a71-8498-67616e214ef2"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
